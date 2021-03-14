fd26b4cc41adf6625adc7397cdcb336e7467294b
0gravity000 <0gravity000@gmail.com>
2021-03-15 01:14:08 +0900

    ログイン、ユーザー登録時のサイドバーの表示不正を修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,52 +97,6 @@
         <div class="py-4">
             <div class="container-fluid">
                 <div class="row">
-                    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                        <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                            <a class="nav-link" href="/">
-                                <span data-feather="home"></span>
-                                TOP
-                            </a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="/meigara">
-                                <span data-feather="file"></span>
-                                銘柄一覧
-                            </a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="/realtime_checking">
-                                <span data-feather="shopping-cart"></span>
-                                リアルタイム銘柄監視
-                            </a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="/realtime_history">
-                                <span data-feather="users"></span>
-                                履歴
-                            </a>
-                            </li>
-                        </ul>
-                    
-                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                            <span>シグナル（日足）</span>
-                            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-                            <span data-feather="plus-circle"></span>
-                            </a>
-                        </h6>
-                        <ul class="nav flex-column mb-2">
-                            <li class="nav-item">
-                            <a class="nav-link" href="/signal">
-                                <span data-feather="file-text"></span>
-                                シグナル（日足）
-                            </a>
-                            </li>
-                        </ul>
-                        </div>
-                    </nav>
-                    
                     @yield('content')
                 </div>
             </div>
--- resources/views/signal_kurosanpei.blade.php
+++ resources/views/signal_kurosanpei.blade.php
@@ -2,19 +2,21 @@
 
 @section('content')
 
+@include('layouts.sidebar')
+
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">シグナル（日足）</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
             <button type="button" class="btn btn-sm btn-outline-secondary">
+              <a href="/signal_volume">出来高急増</a>
+            </button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">
               <a href="/signal_akasanpei">赤三兵</a>
             </button>
             <button type="button" class="btn btn-sm btn-outline-secondary">
               <a href="/signal_kurosanpei">黒三兵</a>
-            </button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">
-              <a href="/signal_volume">出来高急増</a>
             </button>
           </div>
         </div>
